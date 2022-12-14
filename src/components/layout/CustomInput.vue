e210091c9d481f3505c069085632d69042dae736
SurenNeware <surendra.neware@gmail.com>
2022-12-14 16:58:40 +0530

    Added custom input component

---
+++ src/components/layout/CustomInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <label :class="labelClasses ? labelClasses : 'block text-sm font-normal text-grey-900 text-start'" :for="id">{{ labelText }}</label>
+  <input
+    :value="modelValue"
+    @change="onChange"
+    :id="id"
+    :class="inputClasses ? inputClasses : 'block w-full appearance-none rounded-md border bg-transparent border-grey-300 px-3 py-3 placeholder-grey-400 shadow-sm focus:border-peach focus:outline-none focus:ring-peach sm:text-sm invalid:bg-red/5 invalid:border-red peer'"
+  />
+</template>
+
+<script setup>
+const props = defineProps({
+  id: String,
+  labelText: String,
+  modelValue: String,
+  // Input event
+  onChange: {
+    type: Function,
+    required: true,
+  },
+  // Input box classes
+  inputClasses: {
+    type: String,
+    required: true
+  },
+  // Label classes
+  labelClasses: {
+    type: String,
+    required: true
+  }
+})
+</script>
