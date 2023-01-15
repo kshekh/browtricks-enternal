0cb5527641226fce3a9fc293bf7f34b3b707702c
Mohammmad Kamran Shekh <kshekh@gmail.com>
2023-01-15 21:38:10 +0530

    fixing design changes

--- src/components/PmuFormCreate.vue
+++ src/components/PmuFormCreate.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative flex flex-col w-full">    
 
-      <div class="flex-1 space-y-6 pr-16">
+      <div class="flex-1 space-y-6 xs:pr-16">
         <div class="overflow-hidden rounded-lg shadow-3 bg-white border border-grey-300 p-4 lg:py-6 lg:px-8">
             <div class="space-y-4">
                 <div class="relative">
@@ -21,33 +21,16 @@
             </div>         
          
         </div>
-        <div class="relative rounded-lg shadow-3 bg-white border border-grey-300 p-4 lg:py-6 lg:px-8">
-          <!-- Start right side action button -->
-          <div class="absolute top-0 rounded-lg shadow-3 bg-white border border-grey-300 -right-16">
-      
-              <div class="p-1.5">
-                <Button @click="isMoreQuestions = true" class="text-grey-700 hover:bg-red/10 px-2 gap-x-2 inline-flex items-center ml-auto justify-end" :btn-no-border="true" :btn-primary="false">
-                  <PlusCircle class="w-5"/>
-                </Button>
-                <div class="border-t border-primary my-1"></div>
-                <Button class="text-grey-700 hover:bg-red/10 px-2 gap-x-2 inline-flex items-center ml-auto justify-end" :btn-no-border="true" :btn-primary="false">
-                  <PictureIcon class="w-5"/>
-                </Button>
-              </div>
-         
-          </div>
-          <!-- End right side action button -->
-          <div class="space-y-4">
+        <div class="relative space-y-3 xs:space-y-0">
+          <!-- Before Form Upload -->
+        <div class="relative rounded-lg shadow-3 bg-white border border-grey-300 p-4 lg:py-6 lg:px-8">         
+            <div class="space-y-4">
                 <div class="space-y-1">
                   <h3 class="flex-1 text-base font-bold leading-snug text-grey-800">Use your own document</h3>
                   <p class="text-base font-normal text-grey-700 leading-snug md:leading-none">Upload your own pre-built PDF or DOC form</p>
                 </div>
-                <div @click="isUploadDocument = true" class="cursor-pointer flex items-center justify-center gap-5 border border-dashed border-grey-300 p-5 py-14 rounded-xl">
-                 
-                 
-                 
-                 
-                  <div class="flex flex-col">
+                <div @click="isUploadDocument = true" class="cursor-pointer flex items-center justify-center gap-5 border border-dashed border-grey-300 p-5 py-14 rounded-xl">               
+                <div class="flex flex-col">
                     <span class="inline-flex w-full justify-center items-center rounded-md border border-peach-500 bg-peach-500 py-1 px-2 h-full text-xs font-semibold text-grey-800/70 shadow-sm hover:bg-peach-100 hover:border-peach-100 ease-in-out duration-300 cursor-pointer" >
                       <span v-if="value">{{ value.name }}</span>
                       <span v-else>Upload File </span>
@@ -62,14 +45,52 @@
                     Delete
                   </Button>
                 </div>
-          </div>               
+            </div>               
+        </div>
+        <!-- After Form Upload (Please remove class "hidden" when document will be uploaded and updated here)-->
+        <div class="relative rounded-lg shadow-3 bg-white border border-grey-300 space-y-3 p-4 lg:py-6 lg:px-8 hidden">
+                  <div class="flex items-center">
+                      <div class="relative flex-1">
+                        <Input
+                          id="formDescription"
+                          type="text"
+                          :label-text="'Form description'"
+                          :placeholder="'Please read the folowing'" 
+                        />
+                      </div>
+                      <div class="pt-5 flex pl-3 gap-0.5">
+                         
+                        <Button  class="text-red hover:bg-red/10 mv:px-3 gap-x-1.5 inline-flex items-center ml-auto justify-end" :btn-no-border="true" :btn-primary="false">
+                          <TrashIcon class="w-4"/>
+                        </Button>
+                      </div>
+                  </div>
+
+
+                  <div class="relative py-6 px-4 flex justify-center items-center">
+                    <img class="h-auto object-cover" :src="uploadedPdf" alt="" />
+                  </div>
         </div>
 
+        <!-- Start right side action button -->         
+        <div class="xs:absolute xs:top-0 rounded-lg shadow-3 bg-white border border-grey-300 xs:-right-16 inline-flex">      
+              <div class="p-1.5 flex xs:flex-col gap-2 divide-x xs:divide-y xs:divide-x-0 divide-grey">
+                <Button @click="isMoreQuestions = true" class="text-grey-700 hover:bg-red/10 px-2 gap-x-2 inline-flex items-center justify-end" :btn-no-border="true" :btn-primary="false">
+                  <PlusCircle class="w-5"/>
+                </Button>
+                 
+                <Button class="text-grey-700 hover:bg-red/10 px-2 gap-x-2 inline-flex items-center justify-end" :btn-no-border="true" :btn-primary="false">
+                  <PictureIcon class="w-5"/>
+                </Button>
+              </div>
+        </div>
+        <!-- End right side action button -->
+        </div>
         <!-- Start Question area -->
         <div v-if="isMoreQuestions" class="relative rounded-lg shadow-3 bg-white">
           <div class="setup-guide-area p-4 lg:py-6 lg:px-8">
-            <div class="md:grid md:grid-cols-12 md:gap-6">
-              <div class="md:col-span-9">
+            <div class="flex flex-col lg:grid lg:grid-cols-12 gap-3 2xl:gap-6">
+              <div class="lg:col-span-9">
                 <div class="relative space-y-4">
                   
                     
@@ -104,11 +125,11 @@
                           autocomplete="formName"
                         />
                       </div>
-                      <div class="ml-4 mt-4 pt-1">
-                        <Button class="mr-1 hover:bg-red/10 px-0 xs:px-3 gap-x-1.5 inline-flex items-center ml-auto justify-end" :btn-no-border="true" :btn-primary="false">
+                      <div class="pt-5 flex pl-3 gap-0.5">
+                        <Button class=" hover:bg-red/10 mv:px-3 gap-x-1.5 inline-flex items-center ml-auto justify-end" :btn-no-border="true" :btn-primary="false">
                           <PictureIcon class="w-4 text-grey-700"/>
                         </Button>
-                        <Button @click="addOptions--" class="text-red hover:bg-red/10 px-0 xs:px-3 gap-x-1.5 inline-flex items-center ml-auto justify-end" :btn-no-border="true" :btn-primary="false">
+                        <Button @click="addOptions--" class="text-red hover:bg-red/10 mv:px-3 gap-x-1.5 inline-flex items-center ml-auto justify-end" :btn-no-border="true" :btn-primary="false">
                           <TrashIcon class="w-4"/>
                         </Button>
                       </div>
@@ -122,7 +143,7 @@
                 </div>
               </div>
               <div class="md:col-span-3">
-                <div class="flex flex-col h-full">
+                <div class="flex flex-col h-full gap-2">
                   <SelectItem />
                   <div class="mt-auto text-right">
                     <Button class="text-red hover:bg-red/10 px-0 xs:px-3 gap-x-1.5 inline-flex items-center ml-auto justify-end" :btn-no-border="true" :btn-primary="false">
@@ -161,6 +182,7 @@
   import TrashIcon from '@/assets/icons/trash.vue';
   import PlusCircle from '@/assets/icons/PlusCircle.vue';
   import PictureIcon from '@/assets/icons/PictureIcon.vue';
+  import uploadedPdf from '@/assets/Pre_and_Post_Care_Agreement.png';
   const addOptions = ref(1);
   const isMoreQuestions = ref(false);
   const isUploadDocument = ref(false);
